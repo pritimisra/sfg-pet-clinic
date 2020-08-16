e6d8e26b442d1a8ce4d54c263291989f7ef59cfc
DESKTOP-9K8H1QT\priti <pritimisra@gmail.com>
2020-08-16 12:17:47 +0400

    testing the git config

---
+++ src/main/java/priti/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package priti.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
